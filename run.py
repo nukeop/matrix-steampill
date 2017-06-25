82873abe3e07622eb45b777e757733761fed132c
nukeop <alsw@protonmail.com>
2017-06-25 18:53:38 +0200

    Basic event logger for matrix

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from steampill import app
+from steampill import log
+from steampill import version
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+    logger = log.configure_logging('steampill.log')
+    logger.info("Starting matrix-steampill version {}.{}.{}".format(
+        *version.VERSION
+    ))
+
     app.run()
--- steampill/__init__.py
+++ steampill/__init__.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import logging
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
 
 @app.route('/transactions/<transaction>', methods=['PUT'])
 def on_event(transaction):
-    pass
+    events = request.get_json()["events"]
+    for event in events:
+        logger.info("User: {} Room: {}".format(event["user_id"], event["room_id"]))
+        logger.info("Event type: {}".format(event["type"]))
+        logger.info("Content: {}".format(event["content"]))
+    return jsonify({})
